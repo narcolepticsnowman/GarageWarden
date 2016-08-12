3b0c07643efc928682c5ec121d079edea007f304
Robert <r@snow87.com>
2016-08-12 00:28:42 -0600

    fixed bugs

--- garageControl/apps.py
+++ garageControl/apps.py
@@ -12,8 +12,7 @@ class GarageControlConfig(AppConfig):
     def ready(self):
         if not self.is_ready:
             self.is_ready = True
-            GPIO.cleanup()
-            signal.signal(signal.SIGINT, GPIO.cleanup)
+            signal.signal(signal.SIGINT, cleanup_gpio)
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(settings.GARAGE_RELAY_PIN, GPIO.OUT)
             GPIO.setup(settings.FULL_CLOSE_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -22,3 +21,7 @@ class GarageControlConfig(AppConfig):
                                   bouncetime=200)
             GPIO.add_event_detect(settings.FULL_OPEN_SWITCH_PIN, GPIO.RISING, callback=control.update_last_contact,
                                   bouncetime=200)
+
+
+def cleanup_gpio(signal, frame):
+    GPIO.cleanup()
--- garageControl/control.py
+++ garageControl/control.py
@@ -2,7 +2,7 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import HttpResponse, JsonResponse
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 from . import status
 from GarageWarden import settings
@@ -10,7 +10,8 @@ import logging
 import RPi.GPIO as GPIO
 import time
 
-last_contact = datetime.now()
+# init this to now minus the needed time so the requests work as soon as the system starts up
+last_contact = datetime.now() - timedelta(seconds=settings.MIN_TIME_BETWEEN_REQUESTS)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -45,9 +46,9 @@ class ControlView(View):
 
 def trigger_door():
     # close the relay temporarily then re-open it to trigger the door to close
-    GPIO.input(settings.GARAGE_RELAY_PIN, True)
+    GPIO.output(settings.GARAGE_RELAY_PIN, True)
     time.sleep(settings.GARAGE_RELAY_TRIGGER_LENGTH)
-    GPIO.input(settings.GARAGE_RELAY_PIN, False)
+    GPIO.output(settings.GARAGE_RELAY_PIN, False)
     pass
 
 
