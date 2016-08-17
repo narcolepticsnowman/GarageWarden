2a38ac4ee9461bbcb826724873494e6b2b4ef1b4
Robert <r@snow87.com>
2016-08-17 01:17:45 -0600

    added base ui files for app added cors app to djano app to make requests in js work

---
+++ ui/js/gw-app.js
@@ -0,0 +1,20 @@
+var app = angular.module('garage-warden', ['ngRoute']);
+app.controller('statusController', ['$scope','$http', function($scope,$http){
+    $scope.garageFullOpen = null;
+    $scope.garageFullClose = null;
+
+    $http.get("http://10.1.1.16:8000/garage/status").then(function(response){
+        $scope.garageFullClose = response.data.garageFullClose;
+        $scope.garageFullOpen = response.data.garageFullOpen;
+    });
+}]);
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when("/", {
+            redirectTo : "status"
+        })
+        .when("/status", {
+            templateUrl : "templates/status.html",
+            controller: 'statusController'
+        });
+}]);
