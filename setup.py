5b0743f341ea452228edcf5aba4cf5f2e7f07ac8
Robert <r@snow87.com>
2016-08-24 02:50:29 -0600

    made setup not force a new super user creation

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import pip
 import os
-
+os.environ['DJANGO_SETTINGS_MODULE'] = "GarageWarden.settings"
 pip.main(['install', '-r', str(os.path.abspath("requirements.txt"))])
 
+import django
+django.setup()
 from django.core.management import execute_from_command_line
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GarageWarden.settings")
+from django.contrib.auth.models import User
+
 execute_from_command_line(["manage.py", "migrate"])
-execute_from_command_line(["manage.py", "createsuperuser"])
+create_super = False
+if User.objects.filter(is_superuser=True).exists():
+    i = input("There is already at least one super user, would you like to create another (type y or n):")
+    if i and i.lower()[0] == 'y':
+        create_super = True
+if create_super:
+    execute_from_command_line(["manage.py", "createsuperuser"])
